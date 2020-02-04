3fbf2fd63d1cf89cf2deab146d77ffc9bec778ed
jin ze su <jinzesu@192.168.0.102>
2020-02-04 22:12:35 +0800

    user before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
 	before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+	before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
 	def index
 		@groups = Group.all
@@ -24,19 +25,13 @@ class GroupsController < ApplicationController
 	end
 
 	def edit
-		@group = Group.find(params[:id])
-		if current_user != @group.user 
-			redirect_to root_path, alert: "You have no permisstion."
-			
-		end
+		
 	end
 
 	def update
-		@group = Group.find(params[:id])
-		if current_user != @group.user 
-			redirect_to root_path, alert: "You have no permisstion."
+		
 			
-		end
+		
 		if @group.update(group_params)
 			redirect_to groups_path, notice: "update success"
 		else	
@@ -46,16 +41,21 @@ class GroupsController < ApplicationController
 	end
 
 	def destroy
-		@group = Group.find(params[:id])
-		if current_user != @group.user 
-			redirect_to root_path, alert: "You have no permisstion."
-		end
+		 
+
 		@group.destroy
 		flash[:alert] = "Group delete"
 		redirect_to groups_path
 	end
 
 	private
+
+	def find_group_and_check_permission
+		@group = Group.find(params[:id])
+		if current_user != @group.user 
+			redirect_to root_path, alert: "You have no permisstion."
+		end
+	end
 
 	def group_params
 		params.require(:group).permit(:title, :description)
